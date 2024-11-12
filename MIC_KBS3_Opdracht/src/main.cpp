b9e14158ab61d65e11fc81b6e1310dc328d08db3
Quosar <150680941+Quosar@users.noreply.github.com>
2024-11-12 14:28:32 +0100

    Added Screen Functionality
    CHANGE VOID SETUP/LOOP TO INT MAIN

--- MIC_KBS3_Opdracht/src/main.cpp
+++ MIC_KBS3_Opdracht/src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define TFT_CLK 13
+#define TFT_MISO 12
+#define TFT_MOSI 11
+#define TFT_DC 9
+#define TFT_CS 10
+#define TFT_RST 8
+
+#define BLACK    0x0000
+#define BLUE     0x001F
+#define RED      0xF800
+#define GREEN    0x07E0
+#define CYAN     0x07FF
+#define MAGENTA  0xF81F
+#define YELLOW   0xFFE0 
+#define WHITE    0xFFFF
+
+/*
+int main() {
+  Adafruit_ILI9341 screen = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+  Serial.begin(9600);
+
+  screen.begin();
+
+  screen.fillRect(50, 50, 10, 10, BLACK);
+
+  screen.drawRect(50, 50, 10, 10, BLACK);
+  
+  return 0;
+}
+*/
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Adafruit_ILI9341 screen = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+  Serial.begin(9600);
+
+  screen.begin();
+
+  screen.fillRect(50, 50, 10, 10, BLACK);
+
+  screen.drawRect(50, 50, 25, 25, BLACK);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
