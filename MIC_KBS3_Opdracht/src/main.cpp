70294d3e8dbb5b74aef4ae1aafc45d33f293dc54
GerritScholten <146338174+GerritScholten@users.noreply.github.com>
2024-11-14 13:40:32 +0100

    refactor
    Naar gewenste setup en main omgeschreven

--- MIC_KBS3_Opdracht/src/main.cpp
+++ MIC_KBS3_Opdracht/src/main.cpp
@@ -5,6 +5,7 @@ const int NUNCHUCK_ADDRESS = 0x52;
 NunChuk nunchuck;
 
 void setup() {
+  init();
   Serial.begin(9600);
   nunchuck.begin(NUNCHUCK_ADDRESS);
 }
@@ -12,14 +13,22 @@ void setup() {
 uint8_t getNunchuckXAxis() { return nunchuck.state.joy_x_axis; }
 uint8_t getNunchuckYAxis() { return nunchuck.state.joy_y_axis; }
 
-void loop() {
-  if (nunchuck.getState(NUNCHUCK_ADDRESS)) {
-    Serial.print("X axis pos: ");
-    Serial.println((int)getNunchuckXAxis());
-    Serial.print("Y axis pos: ");
-    Serial.println((int)getNunchuckYAxis());
-  } else {
-    Serial.println("Foutje bij lezen?!");
+int main() {
+  setup();
+  while (1)
+  {
+    if (nunchuck.getState(NUNCHUCK_ADDRESS)) {
+      Serial.print("X axis pos: ");
+      Serial.println((int)getNunchuckXAxis());
+      Serial.print("Y axis pos: ");
+      Serial.println((int)getNunchuckYAxis());
+      Serial.println("------------------------");
+      Serial.flush();
+    }
+    else
+    {
+      Serial.println("Foutje bij lezen?!");
+    }
+    _delay_ms(500);
   }
-  delay(1000);
 }
