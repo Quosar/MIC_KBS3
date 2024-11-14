998eb8ac3c5c9180f39c9b65f26c3b62ec48b832
Quosar <150680941+Quosar@users.noreply.github.com>
2024-11-14 13:14:29 +0100

    Updated LCD Code
    Changed defines + Added comments

--- MIC_KBS3_Opdracht/src/main.cpp
+++ MIC_KBS3_Opdracht/src/main.cpp
@@ -4,6 +4,7 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
 
+// LCD Pin Defines
 #define TFT_CLK 13
 #define TFT_MISO 12
 #define TFT_MOSI 11
@@ -11,25 +12,7 @@
 #define TFT_CS 10
 #define TFT_RST 8
 
-//#define TFT_WIDTH 240
-//#define TFT_HEIGHT 320
-
-#define PADDLE_OFFSET_X 6
-#define PADDLE_OFFSET_Y 6
-
-const uint8_t TFT_WIDTH = 240;
-const uint16_t TFT_HEIGHT = 320;
-
-/*
-#define TFT_D0        34 // Data bit 0 pin (MUST be on PORT byte boundary)
-#define TFT_WR        26 // Write-strobe pin (CCL-inverted timer output)
-#define TFT_DC        10 // Data/command pin
-#define TFT_CS        11 // Chip-select pin
-#define TFT_RST       24 // Reset pin
-#define TFT_RD         9 // Read-strobe pin
-#define TFT_BACKLIGHT 25
-*/
-
+// Color defines
 #define BLACK    0x0000
 #define BLUE     0x001F
 #define RED      0xF800
@@ -39,61 +22,73 @@ const uint16_t TFT_HEIGHT = 320;
 #define YELLOW   0xFFE0 
 #define WHITE    0xFFFF
 
-//Adafruit_ILI9341 screen = Adafruit_ILI9341(TFT_CS, TFT_DC);
+// LCD Display Width and Height Limits
+const uint16_t TFT_WIDTH = 240;
+const uint16_t TFT_HEIGHT = 320;
 
+// Offset to set centre of ball from top-left corner
+const uint8_t BALL_OFFSET_X = 6;
+const uint8_t BALL_OFFSET_Y = 6;
 
+// Main
 int main() {
-  Serial.begin(9600);
-  while(!Serial);
-  Serial.println("ILI9341 Test!"); 
+  
+  Serial.begin(9600);   // Start Serial
+  while(!Serial);       // Wait for Start Serial
 
+  // Create Screen Object
   Adafruit_ILI9341 screen = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
+  // Initialize Screen
   screen.begin();
 
-  Serial.println("Screen booted");
-
+  // Set Screen color to Black
   screen.fillScreen(BLACK);
 
+  // Direction and Position Vars for Ball Position
   uint8_t dirX = 1;
   uint8_t dirY = 1;
   uint8_t posX = 1;
   uint8_t posY = 1;
 
+  // Last X/Y position of Ball
   uint16_t lastPosX = 1;
   uint16_t lastPosY = 1;
-
-  //screen.fillRect(200, 300, 20, 20, BLUE);
 
   // Superloop
   while (1) {
     for (int i=0; i < 999; i++) {
+
+      // If Ball is beyond limits of X, Reverse Direction; Else dont
       if (posX >= TFT_WIDTH / 2) {
         dirX = -1;
       } else if (posX <= 1) {
         dirX = 1;
       }
 
+      // If Ball is beyond limits of Y, Reverse Direction; Else dont
       if (posY >= TFT_HEIGHT / 2) {
-        //Serial.println("no");
         dirY = -1;
       } else if (posY <= 1) {
         dirY = 1;
       }
 
+      // Add directional vector to position
       posX += dirX;
       posY += dirY;
 
-      //Serial.println(posY);
+      // Make the last position of the ball black
+      screen.fillRect(lastPosX, lastPosY, BALL_OFFSET_X * 2, BALL_OFFSET_Y * 2, BLACK);
 
-      screen.fillRect(lastPosX, lastPosY, PADDLE_OFFSET_X * 2, PADDLE_OFFSET_Y * 2, BLACK);
-
-      screen.fillRect(posX * 2 - PADDLE_OFFSET_X, posY * 2 - PADDLE_OFFSET_Y, PADDLE_OFFSET_X * 2, PADDLE_OFFSET_Y * 2, RED);
+      // Create the new ball
+      screen.fillRect(posX * 2 - BALL_OFFSET_X, posY * 2 - BALL_OFFSET_Y, BALL_OFFSET_X * 2, BALL_OFFSET_Y * 2, RED);
       
-      lastPosX = posX * 2 - PADDLE_OFFSET_X;
-      lastPosY = posY * 2 - PADDLE_OFFSET_Y;
+      // Set last position to current
+      lastPosX = posX * 2 - BALL_OFFSET_X;
+      lastPosY = posY * 2 - BALL_OFFSET_Y;
       
-      //Serial.println("Drawn Square");
+      // Wait 10ms
+      // TODO: Remove this in favor of timer
       _delay_ms(10);
     }
   }
