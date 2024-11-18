ed1cddc1f2e6706c6532aea5eb7846cc1017120a
GerritScholten <146338174+GerritScholten@users.noreply.github.com>
2024-11-18 19:28:36 +0100

    Nunchuk positie is met bal weergegeven op scherm
    Heb het voor elkaar gekregen dat de positie van de nunchuck weergegeven wordt op het scherm met de bal van Joshua. De issue waar het scherm niet meer werkte zodra we de nunchuck aansloten is gefixt

--- MIC_KBS3_Opdracht/src/main.cpp
+++ MIC_KBS3_Opdracht/src/main.cpp
@@ -1,8 +1,9 @@
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include "Nunchuk.h"
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
-#include "Adafruit_GFX.h"
-#include "Adafruit_ILI9341.h"
 
 // LCD Pin Defines
 #define TFT_CLK 13
@@ -13,86 +14,87 @@
 #define TFT_RST 8
 
 // Color defines
-#define BLACK    0x0000
-#define BLUE     0x001F
-#define RED      0xF800
-#define GREEN    0x07E0
-#define CYAN     0x07FF
-#define MAGENTA  0xF81F
-#define YELLOW   0xFFE0 
-#define WHITE    0xFFFF
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
 
 // LCD Display Width and Height Limits
 const uint16_t TFT_WIDTH = 240;
 const uint16_t TFT_HEIGHT = 320;
 
-// Offset to set centre of ball from top-left corner
+// Offset to set center of ball from top-left corner
 const uint8_t BALL_OFFSET_X = 6;
 const uint8_t BALL_OFFSET_Y = 6;
 
-// Main
+const int NUNCHUCK_ADDRESS = 0x52;
+NunChuk nunchuck;
+
+// Function to map joystick value to screen coordinates
+uint16_t mapJoystickToScreen(uint8_t joystickValue, uint16_t screenSize) {
+  return map(joystickValue, 0, 255, BALL_OFFSET_X, screenSize - BALL_OFFSET_X);
+}
+
 int main() {
-  
-  Serial.begin(9600);   // Start Serial
-  while(!Serial);       // Wait for Start Serial
+  init();
+  Serial.begin(9600); // Start Serial
+
+  // Initialize I2C for Nunchuk
+  Wire.begin();
+  // Initialize the Nunchuk
+  if (!nunchuck.begin(NUNCHUCK_ADDRESS)) {
+    nunchuck.begin(NUNCHUCK_ADDRESS);
+  }
 
   // Create Screen Object
-  Adafruit_ILI9341 screen = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+  Adafruit_ILI9341 screen =
+      Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
   // Initialize Screen
   screen.begin();
-
-  // Set Screen color to Black
   screen.fillScreen(BLACK);
 
-  // Direction and Position Vars for Ball Position
-  uint8_t dirX = 1;
-  uint8_t dirY = 1;
-  uint8_t posX = 1;
-  uint8_t posY = 1;
+  // Ball Position
+  uint16_t posX = TFT_WIDTH / 2;
+  uint16_t posY = TFT_HEIGHT / 2;
 
   // Last X/Y position of Ball
-  uint16_t lastPosX = 1;
-  uint16_t lastPosY = 1;
+  uint16_t lastPosX = posX;
+  uint16_t lastPosY = posY;
 
   // Superloop
   while (1) {
-    for (int i=0; i < 999; i++) {
+    // Update Nunchuk state
+    if (nunchuck.getState(NUNCHUCK_ADDRESS)) {
+      // Get joystick positions
+      uint8_t joyX = nunchuck.state.joy_x_axis;
+      uint8_t joyY = nunchuck.state.joy_y_axis;
 
-      // If Ball is beyond limits of X, Reverse Direction; Else dont
-      if (posX >= TFT_WIDTH / 2) {
-        dirX = -1;
-      } else if (posX <= 1) {
-        dirX = 1;
-      }
+      // Map joystick values to screen coordinates
+      posX = mapJoystickToScreen(joyX, TFT_WIDTH);
+      posY = mapJoystickToScreen(joyY, TFT_HEIGHT);
 
-      // If Ball is beyond limits of Y, Reverse Direction; Else dont
-      if (posY >= TFT_HEIGHT / 2) {
-        dirY = -1;
-      } else if (posY <= 1) {
-        dirY = 1;
-      }
+      // Clear the last ball position
+      screen.fillRect(lastPosX - BALL_OFFSET_X, lastPosY - BALL_OFFSET_Y,
+                      BALL_OFFSET_X * 2, BALL_OFFSET_Y * 2, BLACK);
 
-      // Add directional vector to position
-      posX += dirX;
-      posY += dirY;
+      // Draw the ball at the new position
+      screen.fillRect(posX - BALL_OFFSET_X, posY - BALL_OFFSET_Y,
+                      BALL_OFFSET_X * 2, BALL_OFFSET_Y * 2, RED);
 
-      // Make the last position of the ball black
-      screen.fillRect(lastPosX, lastPosY, BALL_OFFSET_X * 2, BALL_OFFSET_Y * 2, BLACK);
-
-      // Create the new ball
-      screen.fillRect(posX * 2 - BALL_OFFSET_X, posY * 2 - BALL_OFFSET_Y, BALL_OFFSET_X * 2, BALL_OFFSET_Y * 2, RED);
-      
-      // Set last position to current
-      lastPosX = posX * 2 - BALL_OFFSET_X;
-      lastPosY = posY * 2 - BALL_OFFSET_Y;
-      
-      // Wait 10ms
-      // TODO: Remove this in favor of timer
-      _delay_ms(10);
+      // Update last position
+      lastPosX = posX;
+      lastPosY = posY;
     }
+
+    // Small delay to avoid overwhelming updates
+    delay(10);
   }
-  
+
   // Never reach
   return 0;
 }
